1501cd3bbb14670e430361b491546e3ed251e8c5
18mac-02 <18mac-02@pc18mac-02.local>
2019-11-07 12:56:43 +0900

    front

--- dataBase/get.php
+++ dataBase/get.php
@@ -1,6 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 echo json_encode([
-    'kanji' => ['この漢字を書け? りんご', '二', '三', '四', '五'],
+    'kanji' => ['’おお’きないえにすんでいる。', '二', '三', '四', '五'],
     'num' => [1, 2, 3, 4, 5],
 ]);
